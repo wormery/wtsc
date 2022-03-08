01a1f5c9d36f9807e77482faa933594107073c41
meke <mekefly@qq.com>
2022-03-09 01:50:43 +0800

    feat: doc update

--- src/parsers/interface/flex/flexGrow.ts
+++ src/parsers/interface/flex/flexGrow.ts
@@ -1,5 +1,6 @@
 import { GlobalCSSValues } from '../../../CSSValue/types'
+import { ToString } from '../../../core/WTSC/types'
 export type FlexGrow = GlobalCSSValues | number
 export interface FlexGrowInterface {
-  flexGrow(flexGrow: FlexGrow): PRV
+  flexGrow(flexGrow: FlexGrow): ToString
 }
